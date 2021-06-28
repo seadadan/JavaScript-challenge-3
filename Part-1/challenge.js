6c1d4d00ae523b8fefcacde247277d571f8f0ebd
seadadan <siciidcawad@gmail.com>
2021-06-28 11:15:26 +0300

    finish it also added html

--- Part-1/challenge.js
+++ Part-1/challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+var check=500;
+
+
+if (check<=300){
+	console.log(`the bill was ${check},the tip was ${0.15*check} and total was ${(0.15*check)+check} `);
+}
+else if (check>300){
+	console.log(`the bill was ${check},the tip was ${0.20*check} and total was ${(0.20*check)+check} `);
+}
+else{
+	console.log("no tip")
+}
